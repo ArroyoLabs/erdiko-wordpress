a67074917dac2eeecae13b0fc714530111b62d49
colemantung <cmtkusc@gmail.com>
2014-09-05 11:57:44 -0700

    Updated Example controller location.

--- src/erdiko/wordpress/controllers/Example.php
+++ src/erdiko/wordpress/controllers/Example.php
@@ -8,7 +8,7 @@
  * @copyright	Copyright (c) 2014, Arroyo Labs, www.arroyolabs.com
  * @author 		John Arroyo, john@arroyolabs.com
  */
-namespace app\controllers;
+namespace erdiko\wordpress\controllers;
 
 use Erdiko;
 use erdiko\core\Config;
