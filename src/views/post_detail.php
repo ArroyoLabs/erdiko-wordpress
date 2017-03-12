5b3984a0f583ea8855901ab90e43e1db8b3f1b56
john arroyo <john.arroyo@gmail.com>
2017-03-12 00:18:43 -0800

    remove disqus

--- src/views/post_detail.php
+++ src/views/post_detail.php
@@ -26,36 +26,6 @@
                 <?php endif ?>
             </div>
         </div>
-        <!-- Embed comments -->
-        <div class="row">
-            <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-                <div id="disqus_thread"></div>
-            </div>
-        </div>
     </div>
-
-    <script>
-        /**
-        * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-        * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-        */
-        
-        /*
-        var disqus_config = function () {
-            this.page.url = 'http://local.johnarroyo.com/2010/07/issuu-and-uu/'; // Replace PAGE_URL with your page's canonical URL variable
-            this.page.identifier = '481 http://www.johnarroyo.com/?p=481'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        }
-        */
-        
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-
-        s.src = '//johnarroyo.disqus.com/embed.js';
-
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
     
 <?php endif; ?>
